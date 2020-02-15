a996184dcd4b11321796071349df9fa5fc2ca56e
cartermckinnon <cartermckinnon@gmail.com>
2020-02-15 16:03:18 -0700

    logging, gitignore.

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,11 @@ fn main() {
         ap.set_description("Dispatch tasks from a delayed task queue.");
         ap.refer(&mut verbose)
             .add_option(&["-v", "--verbose"], StoreTrue, "Verbose output");
-        ap.refer(&mut redis)
-            .add_option(&["-u", "--redis-url"], Store, "Redis URL (default: 'redis://localhost:6379').");
+        ap.refer(&mut redis).add_option(
+            &["-u", "--redis-url"],
+            Store,
+            "Redis URL (default: 'redis://localhost:6379').",
+        );
         ap.refer(&mut delayed).add_option(
             &["-d", "--delayed-task-zset-key"],
             Store,
@@ -51,62 +54,97 @@ fn main() {
     })
     .expect("Error setting CTRL-C handler");
 
-    println!("Dispatching up to {} tasks per batch from ZSET '{}' to LIST '{}'...", &n, &delayed, &ready);
+    println!(
+        "Dispatching up to {} tasks per batch from ZSET '{}' to LIST '{}'...",
+        &n, &delayed, &ready
+    );
 
     let mut now;
     let backoff = [0, 1, 1, 2, 3, 5, 8, 13];
-    let mut b = 0;
-    let mut i = 0;
+    let mut b = 0; // current position in backoff[]
+    let mut seconds_backed_off = 0;
+    let lines_without_header = 50;
+    let mut lines_since_header = 50; // trigger initial headers
+
     while running.load(Ordering::SeqCst) {
         now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_secs();
-        let result: redis::RedisResult<u64> = script.prepare_invoke()
+        let result: redis::RedisResult<(u64, u64, u64)> = script
+            .prepare_invoke()
             .key(&delayed)
             .key(&ready)
             .arg(now)
             .arg(n)
             .invoke(&mut conn);
-        
         match result {
-            Ok(n) => {
-                if n == 0 {
-                    if b < backoff.len() - 1 {
-                        b += 1; // backoff
-                    }
-                    if verbose {
-                        println!("No tasks ready, waiting {} second(s)", backoff[b]);
-                    }
-                } else {
+            Ok(res) => {
+                // res.0 == dispatched
+                // res.1 == delayed
+                // res.2 == ready
+                if res.0 > 0 {
                     b = 0; // reset backoff
-                    if verbose {
-                        println!("Dispatched {} task(s)", n);
-                    }
+                } else if b < backoff.len() - 1 {
+                    b += 1; // backoff
                 }
-            },
+                if verbose {
+                    if lines_since_header == lines_without_header {
+                        println!(
+                            "+---------------+---------------+---------------+---------------+"
+                        );
+                        println!(
+                            "{}",
+                            format!(
+                                "|{:^15}|{:^15}|{:^15}|{:^15}|",
+                                "dispatched", "delayed", "ready", "backing_off"
+                            )
+                        );
+                        println!(
+                            "+---------------+---------------+---------------+---------------+"
+                        );
+                        lines_since_header = 0;
+                    } else {
+                        lines_since_header += 1;
+                    }
+                    println!(
+                        "{}",
+                        format!(
+                            "|{:^15}|{:^15}|{:^15}|{:^15}|",
+                            res.0, res.1, res.2, backoff[b]
+                        )
+                    );
+                }
+            }
             Err(e) => {
                 println!("Failed to dispatch tasks: {}", e);
                 break;
-            },
+            }
         }
+
         // allow the program to terminate quickly after CTRL-C (within 1 second)
-        while i < backoff[b] && running.load(Ordering::SeqCst) {
+        while seconds_backed_off < backoff[b] && running.load(Ordering::SeqCst) {
             std::thread::sleep(one_second);
-            i += 1;
+            seconds_backed_off += 1;
         }
-        i = 0;
+        seconds_backed_off = 0;
+    }
+    if verbose && lines_since_header > 0 {
+        println!("+---------------+---------------+---------------+---------------+");
     }
     println!("Exiting!");
 }
 
 const SCRIPT: &str = r"
 local ready = redis.call('zrangebyscore',KEYS[1], 0, tonumber(ARGV[1]),'limit',0,tonumber(ARGV[2]))
+local dispatched = 0
 if ready ~= nil and #ready > 0 then
   for score,host in pairs(ready) do
     redis.call('rpush', KEYS[2], host)
     redis.call('zrem', KEYS[1], host)
   end
-  return #ready
+  dispatched = #ready
 end
-return 0";
+local delayed = redis.call('zcard', KEYS[1])
+local ready_prev = redis.call('llen', KEYS[2]) - dispatched
+return {dispatched, delayed, ready_prev}";
